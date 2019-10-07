b8bbf52db1ac99466b4dff6ee53cf002c0441d55
“Vaibhav” <vaibhavmisra.1987@gmail.com>
2019-10-07 11:36:26 +0530

    Added timer to QuestionVC.

--- TranlsationQuiz/App/ViewController/QuestionViewController.swift
+++ TranlsationQuiz/App/ViewController/QuestionViewController.swift
@@ -18,19 +18,53 @@ class QuestionViewController : UIViewController, Storyboarded {
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var translationTopConstraint: NSLayoutConstraint!
     
+    var timer: Timer?
+    var timeRemaining = 5
+    
     override func viewDidLoad() {
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
-        if let question = self.question {
-            wordLabel.text = question.word
-            translationLabel.text = question.translation
-        }
+        super.viewDidLoad()
+        configureView()
+        timer = Timer.scheduledTimer(withTimeInterval: 1,
+                                     repeats: true,
+                                     block: { [weak self] timer in
+            self?.timerAction()
+        })
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        timer?.fire()
+    }
+    
+    //MARK: - IBAction methods
     @IBAction func incorrectBtnTapped(_ sender: UIButton) {
+        timer?.invalidate()
         selection?(.incorrect)
     }
     
     @IBAction func correctBtnTapped(_ sender: UIButton) {
+        timer?.invalidate()
         selection?(.correct)
+    }
+    
+    //MARK: - Private Helper
+    private func configureView() {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        if let question = self.question {
+            wordLabel.text = question.word
+            translationLabel.text = question.translation
+        }
+        self.timerLabel.text = "0:0\(timeRemaining)"
+    }
+    
+    private func timerAction() {
+        timeRemaining -= 1
+        if self.timeRemaining < 0 {
+            timer?.invalidate()
+            selection?(.noAnswer)
+        } else {
+            timerLabel.text = "0:0\(timeRemaining)"
+        }
     }
 }
